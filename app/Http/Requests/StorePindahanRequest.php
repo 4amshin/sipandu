c059b9af8ffb897bd2d37af3a273aed8556a8cd6
4amshin <shinmendenz@gmail.com>
2024-06-16 10:18:27 +0800

    Fitur Tambah Data Pindahan

--- app/Http/Requests/StorePindahanRequest.php
+++ app/Http/Requests/StorePindahanRequest.php
@@ -11,7 +11,7 @@ class StorePindahanRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StorePindahanRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nama' => 'required|string|max:255',
+            'jenis_kelamin' => 'required|in:laki-laki,perempuan',
+            'nik' => 'required|string|max:16',
+            'tanggal_pindah' => 'required|date',
+            'alasan_pindah' => 'required|string',
         ];
     }
 }
